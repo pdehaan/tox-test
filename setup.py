752aa9958371134ae808cf2f84dcd92725baba4f
Peter deHaan <peter@deseloper.com>
2016-01-11 16:08:55 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+Hey man this is a really long message to see how flake8 likes lines longer than 80 chars.
+"""
+
+from setuptools import setup
+
+setup(
+  name="sample",
+  version="1.0.0",
+  description="peter smells",
+  license="WTFPL"
+)
